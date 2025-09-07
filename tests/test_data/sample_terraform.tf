449496fcb3e4d22b52551c04f7e9ab4d882d2425
mscherenberg-prodyna <marcel.scherenberg@prodyna.com>
2025-09-07 14:42:59 +0200

    test: removed redundant tests

--- tests/test_data/sample_terraform.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = "ami-12345678"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "example-instance"
-  }
-}
--- tests/unit/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Unit tests for IAC Agents components."""
